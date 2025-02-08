323ed54a9fb816057821a5b784371eaa9a7583cc
Lord Lumineer <56554075+LordLumineer@users.noreply.github.com>
2025-02-08 00:43:44 -0500

    V1.0.5   try it out (#17)
    * feat: :sparkles: v1.0.5 - Try It Out
    
    Added a box to try out the service directly in the home page
    
    * v1.0.5 - BETA
    
    * beta 1
    
    * removed the bridge to the titles and cleaned so of the code
    
    * feat: :test_tube: Better CI/CD to include the pre-release branch
    
    Invalid version on purpose to test the version validation
    
    Invalid version on purpose to test the version validation
    
    * run version check on pre-release PR
    
    * Now with valid version
    
    * beta v2 fixed homepage tags
    
    * fix: :bug: Fixed wrong endpoint for SEO

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ from starlette.middleware.cors import CORSMiddleware
 active_connections: dict[str, WebSocket] = {}
 
 app = FastAPI(
-    version="1.0.5-beta-v2",
+    version="1.0.5-beta-v3",
     docs_url=None,  # Disable Swagger UI
     redoc_url=None  # Disable ReDoc
 )
